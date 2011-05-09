3896244257d7473b8ace13cee99440183a1bd71a
James Alexander <jamesralexander@gmail.com>
2011-05-08 23:41:58 -0400

    Upgrading to django 1.3

--- src/comicSettings.py
+++ src/comicSettings.py
@@ -6,7 +6,7 @@ os.environ['TZ'] = "US/Eastern"
 settings.configure(DEBUG=True, TEMPLATE_DEBUG=True,
 	DATABASES = {
 		'prod': {
-			'ENGINE': 'mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+			'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 			'NAME': '',                      # Or path to database file if using sqlite3.
 			'USER': '',                      # Not used with sqlite3.
 			'PASSWORD': '',                  # Not used with sqlite3.
@@ -14,7 +14,7 @@ settings.configure(DEBUG=True, TEMPLATE_DEBUG=True,
 			'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
 		},
 		'default': {
-			'ENGINE': 'mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+			'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 			'NAME': 'comicdb',                      # Or path to database file if using sqlite3.
 			'USER': 'root',                      # Not used with sqlite3.
 			'PASSWORD': 'root',                  # Not used with sqlite3.
