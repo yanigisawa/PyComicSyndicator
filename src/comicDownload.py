d320f778436483c474299439d2eedf1be2b68459
James Alexander <jamesralexander@gmail.com>
2011-08-19 09:08:41 -0400

    Retabbing indentation. Adding catch-all exception

--- src/comicDownload.py
+++ src/comicDownload.py
@@ -8,64 +8,80 @@ import comicSettings
 from comicdb.models import *
 
 def getLatestComicLog(id):
-	try:
-		prevLog = ComicLog.objects.filter(ComicId=id).latest('FetchDate')
-	except ComicLog.DoesNotExist:
-		prevLog = None
-	return prevLog
+    try:
+        prevLog = ComicLog.objects.filter(ComicId=id).latest('FetchDate')
+    except ComicLog.DoesNotExist:
+        prevLog = None
+    return prevLog
 
 def getCurrentRss(id):
-	try:
-		prevRss = ComicRss.objects.filter(ComicLogId__ComicId=id).latest('ComicLogId__FetchDate')
-	except ComicRss.DoesNotExist:
-		prevRss = None
-	return prevRss
-	
+    try:
+        prevRss = ComicRss.objects.filter(ComicLogId__ComicId=id).latest('ComicLogId__FetchDate')
+    except ComicRss.DoesNotExist:
+        prevRss = None
+    return prevRss
+    
 
 def getHtml(comic):
-	html = ""
-	try:
-		html = htmlFetch.GetHtml(comic.Url)
-	except urllib2.HTTPError, e:
-		print "    Unable to fetch html for Comic: \"%s\" - HTTPCode: %s" % (comic.Name, e.code)
-		print "    URL: %s" % e.geturl()
-	except urllib2.URLError, e:
-		print "    Unable to fetch html for Comic: \"%s\" - UrlErrorReason: %s" % (comic.Name, e.reason)
-		print "    URL: %s" % comic.Url
-	return html
+    html = ""
+    count = 0
+    while html == "" and count < 4:
+        time.sleep(1)
+        try:
+            html = htmlFetch.GetHtml(comic.Url)
+        except urllib2.HTTPError, e:
+            if count == 3:
+                print "    Unable to fetch html for Comic: \"%s\" - HTTPCode: %s" % (comic.Name, e.code)
+                print "    URL: %s" % e.geturl()
+        except urllib2.URLError, e:
+            if count == 3:
+                print "    Unable to fetch html for Comic: \"%s\" - UrlErrorReason: %s" % (comic.Name, e.args)
+                print "    URL: %s" % comic.Url
+        except:
+            if count == 3:
+                print "    Unable to fetch html for Comic: \"%s\" - Unkown Exception: %s" % \
+                    (comic.Name, sys.exc_info()[0])
+        count = count + 1
+
+    return html
 
 def fetchComicImageUrls():
-	rssItems = []
-	for comic in Comic.objects.filter(IsActive=1):
-		time.sleep(1)
-		parser = eval(comic.ParserConstructor)
+    comics = Comic.objects.filter(IsActive=1)
+    for comic in comics:
+        parser = eval(comic.ParserConstructor)
 
-		html = getHtml(comic)
-		prevLog = getLatestComicLog(comic.id)
+        html = getHtml(comic)
 
-		foundUrlsToday = ComicLog.objects.filter(FetchDate__gte=datetime.datetime.utcnow().date(), 
-				ComicId=comic.id).values('ImageUrl').distinct().count()
+        if html == "":
+            continue    
 
-		log = ComicLog()
-		log.ImageUrl = parser.getImageLocation(html)
-		log.ComicId = comic
-		log.FetchDate = datetime.datetime.utcnow()
-		if log.ImageUrl != "":
-			log.save()
-		else:
-			print "Unable to find image for Comic - %s" % comic.Name
+        prevLog = getLatestComicLog(comic.id)
 
-		currentRss = getCurrentRss(comic.id)
+        foundUrlsToday = ComicLog.objects.filter(FetchDate__gte=datetime.datetime.utcnow().date(), 
+                ComicId=comic.id).values('ImageUrl').distinct().count()
 
-		if (prevLog != None and prevLog.ImageUrl != log.ImageUrl and foundUrlsToday <= 1
-				and log.ImageUrl != "") or currentRss == None:
-			log = getLatestComicLog(comic.id)
-			rss = ComicRss()
-			rss.ComicLogId = log
-			rss.save()
+        log = ComicLog()
+        log.ImageUrl = parser.getImageLocation(html)
+        log.ComicId = comic
+        log.FetchDate = datetime.datetime.utcnow()
+        if log.ImageUrl != "":
+            log.save()
+        else:
+            print "Unable to find image for Comic - %s" % comic.Name
+        
+        parser.reset()
+
+        currentRss = getCurrentRss(comic.id)
+
+        if (prevLog != None and prevLog.ImageUrl != log.ImageUrl and foundUrlsToday <= 1
+                and log.ImageUrl != "") or currentRss == None:
+            log = getLatestComicLog(comic.id)
+            rss = ComicRss()
+            rss.ComicLogId = log
+            rss.save()
 
 
 def main():
-	fetchComicImageUrls()
+    fetchComicImageUrls()
 
 main()
